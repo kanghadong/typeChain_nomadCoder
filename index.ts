25dbca316b7cd4c370dc870764d8d94a08e7eefa
kanghadong <hdkang2007@gmail.com>
2018-07-02 19:44:59 +0900

    "first steps with typeScript"

--- index.ts
+++ index.ts
@@ -1,1 +1,12 @@
-console.log('hello');
+const name = "Kang",
+  age = 28,
+  gender = "male";
+
+
+const sayHi = (name, age, gender) => {
+  console.log(`Hello ${name}, you are ${age}, you are a ${gender}`);
+};
+
+sayHi(name, age, gender);
+
+export {};
