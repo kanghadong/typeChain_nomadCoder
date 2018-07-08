f99a81ce7b74c109b8a6735a26ee42b6b2c75592
kanghadong <hdkang2007@gmail.com>
2018-07-08 12:34:56 +0900

    classes on Typescript

--- dist/index.js
+++ dist/index.js
@@ -1,12 +1,15 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const person = {
-    name: "kang",
-    age: 28,
-    gender: 'male',
-};
+class Human {
+    constructor(name, age, gender) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+    }
+}
+const kang = new Human("kang", 28, "male");
 const sayHi = (person) => {
     console.log(`Hello ${person.name}, you are ${person.age}, you are a ${person.gender}`);
 };
-sayHi(person);
+sayHi(kang);
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,21 @@
-interface Human {
-  name: string;
-  age: number;
-  gender: string;
+class Human {
+  public name: string;
+  public age: number;
+  public gender: string;
+
+  constructor(name: string, age: number, gender: string) {
+    this.name = name;
+    this.age = age;
+    this.gender = gender;
+  }
 }
 
-const person = {
-  name: "kang",
-  age: 28,
-  gender: 'male',
-};
+const kang = new Human("kang", 28, "male");
 
 const sayHi = (person: Human): void => {
   console.log(`Hello ${person.name}, you are ${person.age}, you are a ${person.gender}`);
 };
 
-sayHi(person);
+sayHi(kang);
 
 export {};
